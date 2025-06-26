6475e0f372ee9513bf3a911f8739ba2709cca25f
charlesnoah344 <23397@ecam.be>
2025-06-26 16:53:29 +0200

    plus propre

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,13 @@
 import string
 import random
+def password():
+    '''cette fonction retourne un mot de passe sécurisé'''
+    taille=random.randint(12,20)
+    possibilities=string.ascii_letters+string.digits+string.punctuation
+    pass_word=''
+    for i in range(taille):
+        pass_word+=random.choice(possibilities)
+    return pass_word
+print(password())
 
-taille=random.randint(12,20)
-possibilities=string.ascii_letters+string.digits+string.punctuation
-pass_word=''
-for i in range(taille):
-    pass_word+=random.choice(possibilities)
-print(pass_word)
+    
